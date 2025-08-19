35d340c7530dbaada9ebf966e5dbb800c8c49958
Tracienek <baotrann09042004@gmail.com>
2025-08-19 11:31:09 +0700

    feat(ui): adopt Header/Footer and layout from hy_dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,27 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/layouts/Layout";
 import ProfileLayout from "./pages/profile/profileLayout/ProfileLayout";
-import { QueryClient, QueryClientProvider } from 'react-query';
-// import Checkout from './pages/orders/Checkout';
-// import OrderSuccess from './pages/orders/OrderSuccess';
-// import ShipperOrders from './pages/orders/ShipperOrders';
-
-// <Route path="/checkout" element={<Checkout />} />
-// <Route path="/orders/success" element={<OrderSuccess />} />
-// <Route path="/shipper/orders" element={<ShipperOrders />} />
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthLayout from "./pages/auth/authLayout/AuthLayout";
+import AuthForm from "./pages/auth/authForm/AuthForm";
 
 const routes = [
     {
-        path: "",
+        path: "", 
         element: <Layout />,
         children: [
             {
-                path: "/profile",
-                element: <ProfileLayout />,
+                
             },
         ],
     },
+
+    {
+        path: "auth/:mode/:role",
+        element: <AuthLayout />, 
+        children: [{ index: true, element: <AuthForm /> }],
+    },
+
 ];
 
 const router = createBrowserRouter(routes);
