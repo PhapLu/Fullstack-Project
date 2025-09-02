5570a917201b309c3b4e86cc722d58d74b927f83
tghyhy23 <s4053650@rmit.edu.vn>
2025-09-02 17:31:50 +0700

    fix headera & filter  layout

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -23,8 +23,9 @@ export default function Header() {
         try {
             // clear cookie on server (adjust path if your route differs)
             await apiUtils.post("/auth/logout");
-        } catch (_) {
+        } catch (e) {
             // ignore; we'll still clear client state
+            console.log(e)
         } finally {
             dispatch(logout());
             navigate("/"); // optional
