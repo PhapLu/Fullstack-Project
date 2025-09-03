2d3dc6621281a88d7040eceafa752c160788559f
Tracienek <baotrann09042004@gmail.com>
2025-09-04 00:41:48 +0700

    fix product UI in landing page

--- client/src/utils/currency.js
+++ client/src/utils/currency.js
@@ -1,7 +1,6 @@
-export const vnd = (n) =>
-    new Intl.NumberFormat("vi-VN", {
-      style: "currency",
-      currency: "VND",
-      maximumFractionDigits: 0,
-    }).format(Number(n || 0));
-  
+export const usd = (n) =>
+  new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+  }).format(Number(n || 0));
